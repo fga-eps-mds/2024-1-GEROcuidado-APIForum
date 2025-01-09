7b2465600af75a44b1d72eeca156e5b8f9a2c354
jvopBR <Merlinjoaovitor@gmail.com>
2025-01-09 12:08:41 -0300

    Tentando migrar o DB

---
+++ src/data-source.ts
@@ -0,0 +1,14 @@
+import { DataSource } from 'typeorm';
+import { Publicacao } from './publicacao/entities/publicacao.entity'; // Caminho correto para sua entidade
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost', // Host correto do banco de dados
+  port: 5432,
+  username: 'postgres', // Usuário do banco de dados
+  password: 'postgres', // Senha do banco de dados
+  database: 'gerocuidado-forum-db', // Nome do banco
+  entities: [Publicacao], // Lista de entidades
+  migrations: ['src/migration/**/*.ts'], // Caminho para as migrações
+  synchronize: false, // Não sincronizar automaticamente; usar migrações
+});
