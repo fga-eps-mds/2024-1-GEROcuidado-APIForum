88eac0f25700f94366af01a2343ede39dc9a9ea7
jvopBR <Merlinjoaovitor@gmail.com>
2025-02-08 15:49:39 -0300

    Comentario Funcionando Pelo POSTMAN

--- src/publicacao/comentario.controller.ts
+++ src/publicacao/comentario.controller.ts
@@ -2,6 +2,7 @@
 import { BadRequestException, Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { Ordenate, Ordering } from '../shared/decorators/ordenate.decorator';
 import { Paginate, Pagination } from '../shared/decorators/paginate.decorator';
+import { PublicRoute } from '../shared/decorators/public-route.decorator';
 import { ResponsePaginate } from '../shared/interfaces/response-paginate.interface';
 import { ComentariosService } from './comentario.service';
 import { CreateComentarioDto } from './dto/create-comentario.dto';
@@ -9,6 +10,7 @@ import { UpdateComentarioDto } from './dto/update-comentario.dto';
 import { Comentario } from './entities/comentario.entity';
 
 @Controller('comentarios')
+@PublicRoute()
 export class ComentariosController {
   constructor(private readonly comentariosService: ComentariosService) { }
 
